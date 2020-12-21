e348b27249ba2fe8cbe690d551187d2af2326d1c
Fabio Vettori <fabio_vettori@hotmail.it>
2020-12-21 19:40:45 +0100

    installation Lavarel  ok + check jQuery & Bootstrap dependency ok + Route examples  ok + data examples ok

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title title-page="home">Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        {{-- FONTS --}}
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap" rel="stylesheet">
+
+        {{-- CSS --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+        {{-- JS --}}
+        <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
+    </head>
+    <body>
+        <main>
+            <div class="container-lg">
+                <div class="row text-center">
+                    <div class="col-lg-12">
+                        <h1>Homepage</h1>
+                    </div>
+
+                    <div class="col-lg-12">
+                        <h3>Ciao {{ $name }}</h3>
+                    </div>
+                    <div class="col-lg-6">
+                        <h1>col-lg-6</h1>
+                    </div>
+                    <div class="col-lg-6">
+                        <h1>col-lg-6</h1>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
